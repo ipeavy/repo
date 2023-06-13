e3c6216866b2d593714f6242602bc82d41cce834
Ishmael Peavy <ipeavy401@gmail.com>
2023-06-12 23:02:56 -0400

    added function to the ham menu

--- src/js/partials/ham.js
+++ src/js/partials/ham.js
@@ -1,5 +1,11 @@
 const hamburgerMenu = document.querySelector('.ham-container');
+const linkContainer = document.querySelector('.link-container');
 
 hamburgerMenu.addEventListener('click', function() {
   this.classList.toggle('active');
+  linkContainer.classList.toggle('visible');
+  
+  if (!this.classList.contains('active')) {
+    linkContainer.classList.remove('visible');
+  }
 });
