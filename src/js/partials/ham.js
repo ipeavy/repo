55618725ef40a2296bce3c2075317463269dd6e8
Ishmael Peavy <ipeavy401@gmail.com>
2023-06-16 16:28:47 -0400

    finished nav design and changed font color

--- src/js/partials/ham.js
+++ src/js/partials/ham.js
@@ -1,11 +1,14 @@
 const hamburgerMenu = document.querySelector('.ham-container');
 const linkContainer = document.querySelector('.link-container');
+const signupContainer = document.querySelector('.sign-up');
 
 hamburgerMenu.addEventListener('click', function() {
   this.classList.toggle('active');
   linkContainer.classList.toggle('visible');
+  signupContainer.classList.add('black');
   
   if (!this.classList.contains('active')) {
     linkContainer.classList.remove('visible');
+    signupContainer.classList.remove('black');
   }
 });
